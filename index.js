43882a79ff135c3ca953f815356119fef115f9ee
Niru2002 <nirmaychoksi3@gmail.com>
2024-09-13 17:49:14 +0530

    Made changes to firbase initialization

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require('firebase-admin');
 const app = express();
 const port = 3000;
-const admin = require('firebase-admin')
+
+// Initialize Firebase Admin SDK only once
+admin.initializeApp({
+    projectId: "push-notifications-f7c23",
+    serviceAccountId: "101646088545574856608"
+});
 
 // Use body-parser middleware to parse incoming request bodies
 app.use(bodyParser.json());
@@ -13,15 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-    admin.initializeApp({
-        projectId: "push-notifications-f7c23",
-        serviceAccountId: "101646088545574856608"
-    })
     res.send('Hello World!');
 });
 
 app.post('/send', async (req, res) => {
-    const requestData = req.body
+    const requestData = req.body;
     const payload = {
         notification: {
             title: requestData.title,
@@ -31,12 +33,14 @@ app.post('/send', async (req, res) => {
     };
 
     try {
-        const response = await admin.messaging().sendEachForMulticast(payload);
+        const response = await admin.messaging().sendMulticast(payload); // Corrected method name
         console.log(`${response.successCount} messages were sent successfully`);
+        res.status(200).send('Notifications sent successfully.');
     } catch (error) {
         console.error('Error sending notifications:', error);
+        res.status(500).send('Error sending notifications.');
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
